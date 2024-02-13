cca81b832a560df45929cfc94ff70a6ac5e133f2
miguegamer1526 <151801015+miguegamer1526@users.noreply.github.com>
2024-02-12 20:07:07 -0500

    Add files via upload

---
+++ 10/ADV 1_4 C85 Act Alumno/main.js
@@ -0,0 +1,25 @@
+nombre_estudiante_array = []
+
+function submbit()
+{
+
+    var vernombre_estudiante_array = [];
+
+    for (var j = 1; j <= 4; j++ );
+    {
+        var nombre_estudiante - document getElementById("nombre_estudiante_"+j).valude;
+        console.log(nombre_estudiante);
+        nombre_estudiante_array.push(nombre_estudiante);
+    }
+
+    console.log(nombre_estudiante);
+
+    var longitudnombre_estudiante_array = nombre_estudiante_array.leigth;
+    console.log(longitudnombre_estudiante_array);
+
+    for ( var k = 0; k < longitudnombre_estudiante_array; k++);
+    {
+        ver_estudiante_array.push("<h4>NOMBRE _ " nombre_estudiante_array [k] );
+    }
+
+}
